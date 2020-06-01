0e3df78689eeb8bb10ec9e5320c7b7f5da41a3a6
Kammradt <vinicius.kammradt1@gmail.com>
2020-06-01 01:53:50 -0300

    :art: Fixing file with prettier

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
 // @ts-ignore
-import DatetimePicker from "vuetify-datetime-picker";
+import DatetimePicker from 'vuetify-datetime-picker';
 
 Vue.use(DatetimePicker);
 Vue.config.productionTip = false;
@@ -13,5 +13,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
